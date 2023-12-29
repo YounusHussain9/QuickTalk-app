7b2aa6e2003a3947c5483ca9183173f2129d6423
YounusHussain <younushussain0@gmail.com>
2023-12-29 19:15:24 +0500

    sign_in_page

---
+++ app/(public)/signin/form.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React, { FormEvent, useState } from "react";
+
+const Form = () => {
+  const router = useRouter();
+  const [username, setUserName] = useState<undefined | string>("");
+  const [password, setPassword] = useState<undefined | string>("");
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    const res = await fetch("/api/login", {
+      method: "post",
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (res.ok) {
+      router.push("/feed");
+    } else {
+      alert("log in failed");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col bg-slate-800 p-3 max-w-xs w-full rounded">
+      <div className="text-center">
+        <h3 className="p-3 font-semibold">Sign In</h3>
+      </div>
+
+      <div>
+        <hr />
+      </div>
+
+      <div className="flex flex-col gap-5 p-3" >
+        <div className="flex flex-col gap-2">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            onChange={(e) => setUserName(e.target.value)}
+            id="username"
+            placeholder="Username"
+            value={username}
+            required
+            className="p-3 rounded outline-none"
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            id="password"
+            placeholder="Password"
+            value={password}
+            required
+            className="p-3 rounded outline-none"
+
+          />
+        </div>
+        <button type="submit">Login</button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
---
+++ app/(public)/signin/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Form from './form'
+
+const SignIn = () => {
+  return (
+    <div>
+        <Form />
+    </div>
+  )
+}
+
+export default SignIn
