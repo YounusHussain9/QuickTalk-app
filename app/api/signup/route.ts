ebcf80fe82e25cf9d02aff8c5dbb19033dfcc08f
YounusHussain <younushussain0@gmail.com>
2024-01-01 12:45:19 +0500

    sign_up_end_point_added

---
+++ app/api/signup/route.ts
@@ -0,0 +1,25 @@
+import { sql } from "@/db";
+import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
+
+export const POST = async (request: Request) => {
+  const json = await request.json();
+  const res = await sql(
+    "select id, user from public.users where username ilike $1",
+    [json.username]
+  );
+
+  if (res.rowCount > 0) {
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+  }
+
+  const saltRounds = 10;
+  const hashPassword = await bcrypt.hash(json.password, saltRounds);
+
+  await sql("insert into public.users (username, password) values ($1, $2)", [
+    json.username,
+    hashPassword,
+  ]);
+
+  return NextResponse.json({ msg: "Register succesfuly" }, { status: 201 });
+};
