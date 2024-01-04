b339ba9014e377c97d466e9372248b4077a4874c
YounusHussain9 <younushussain0@gmail.com>
2024-01-04 15:40:52 +0500

    Squashed commit of the following:
    commit ea4bf5ee84a8640e276c844036dce8bb5ae88ac4
    Author: YounusHussain9 <younushussain0@gmail.com>
    Date:   Thu Jan 4 15:39:44 2024 +0500
    
        feed_list_work

--- type.ts
+++ type.ts
@@ -2,5 +2,13 @@ interface UserI{
     id: string | number,
     username: string,
     avatar: string
+}
 
+
+interface PostI{
+    id: number,
+    content: string,
+    username: string,
+    avatar: string
+    created_at: string
 }
