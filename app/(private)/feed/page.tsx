57ef758822a5bb14d64ccfbeeba9e4bd2f75130b
YounusHussain9 <younushussain0@gmail.com>
2024-01-04 18:42:14 +0500

    get_post_api_end_point

--- app/(private)/feed/page.tsx
+++ app/(private)/feed/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import FeedContainer from './feed-container'
+import UserProfile from '../../components/Profile/Card'
 
 const Feed = () => {
   return (
     <main>
       <h2>Feed</h2>
+      {/* <UserProfile /> */}
       <FeedContainer />
     </main>
   )
